a2330d3cd16e059b1262b03feaea94b394e4523d
Jhuan <j.pressley87@hotmail.com>
2019-11-01 17:55:20 -0400

    made so changes to prompts

--- src/com/company/Rated_R_AdmissionCalc.java
+++ src/com/company/Rated_R_AdmissionCalc.java
@@ -6,43 +6,56 @@ public class Rated_R_AdmissionCalc {
 
     public static void main(String[] args) {
 
-        System.out.println("What is the age of first patron?");
+        System.out.println("Is there 1 or 2 patrons?");
         Scanner scanner = new Scanner(System.in);
-        String PatronAge = scanner.nextLine();
-        int age = Integer.parseInt(PatronAge);
+        String moreThanOne = scanner.nextLine();
+        int NumbPatrons = Integer.parseInt(moreThanOne);
 
-        System.out.println("What is the age of second patron?");
-        Scanner scanner2 = new Scanner(System.in);
-        String PatronAge2 = scanner2.nextLine();
-        int age2 = Integer.parseInt(PatronAge2);
 
-        if(age2 != 0) {
-            if (age < 17 || age2 < 17) {
+        if(NumbPatrons == 2) {
+            System.out.println("What is the age of first patron?");
+            Scanner scanner1 = new Scanner(System.in);
+            String PatronAge = scanner1.nextLine();
+            int age = Integer.parseInt(PatronAge);
+
+            System.out.println("What is the age of second patron?");
+            Scanner scanner2 = new Scanner(System.in);
+            String PatronAge2 = scanner2.nextLine();
+            int age2 = Integer.parseInt(PatronAge2);
+
+
+                if (age < 17 || age2 < 17) {
                     boolean isYoungerThan17 = age < 17 && age2 < 17 ? true : false;
                     boolean isOlderthan21 = age >= 21 || age2 >= 21 ? true : false;
                     if (isOlderthan21) {
                         System.out.println("Minor is with adult so admission is permitted");
                     }//closes isOlderthan21 if
-                        else if(isYoungerThan17) {
-                            System.out.println("Patrons not old enough to be admitted");
+                    else if (isYoungerThan17) {
+                        System.out.println("Patrons not old enough to be admitted");
                     }//closes if one patron YoungerThan 17 else if
-                        else {
+                    else {
                         System.out.println("Minor party is not of age to be admitted");
-                        } //closes neither party else
+                    } //closes neither party else
                 }//closes either party under 17 if
-            else {
-                System.out.println("Both parties are of age and can be admitted");
+                else {
+                    System.out.println("Both parties are of age and can be admitted");
                 }//closes if 21 else
-            }//close if more than one person
 
-        else if(age2 == 0 ) {
-            if (age >= 17) {
-                    System.out.println("Admission permitted");
+        }//closes number patrons if
+        else if (NumbPatrons == 1){
+
+            System.out.println("What is the age of the patron?");
+            Scanner scanner1 = new Scanner(System.in);
+            String PatronAge = scanner1.nextLine();
+            int age = Integer.parseInt(PatronAge);
+
+         if (age >= 17) {
+                    System.out.println("Admission permitted patron of age");
                 }//closes is older than 17 if
 
             else{
                 System.out.println("Admission not permitted patron too young");
             }//closes age 2 present else
-        }//closes age2 present else if
+        }//closes number patrons else if
     }//closes main
 }//close class
